705d9696b58b8399f300b2afc8c6863abe13a400
AKDEAS\akdea <akdeaso@gmail.com>
2024-05-04 13:21:28 +0700

    add another test case

--- test/specs/register.spec.js
+++ test/specs/register.spec.js
@@ -6,15 +6,36 @@ describe("Test Register", () => {
     const email = await $("#text");
     const password = await $("#password");
     const confirmPassword = await $("#confirmPassword");
+    const checkBox = await $('input[type="checkbox"]');
     const submit = await $('button[type="submit"]');
     const errMsg = await $('small[class="form-message"]');
-    const checkBox = await $('input[type="checkbox"]');
-    await email.addValue("test@test.com");
-    await password.addValue("INIpassword123");
-    await confirmPassword.addValue("paswordtidaksama");
+    await email.setValue("test@test.com");
+    await password.setValue("INIpassword123");
+    await confirmPassword.setValue("paswordtidaksama");
     await checkBox.click();
     await submit.click();
     await expect(errMsg).toHaveText(expect.stringContaining("Ulangi Password"));
+    await browser.pause(3000);
+  });
+  it.only("should appear error validation 2", async () => {
+    await browser.url(
+      "https://contributor-accounts.shutterstock.com/users/new"
+    );
+    await browser.pause(5000);
+    const fullName = await $("#full_name");
+    const userName = await $("#username");
+    const email = await $("#email");
+    const password = await $("#password");
+    // const captcha = await $("#recaptcha-anchor");
+    const submit = await $("#login");
+    const errMsg = await $('li[class="parsley-required"]');
+    await fullName.setValue("this is full name");
+    await userName.setValue("thisisawesome");
+    await email.setValue("test@test.com");
+    await password.setValue("INIpassword123");
+    // await captcha.click();
+    await submit.click();
+    await expect(errMsg).toHaveText(expect.stringContaining("Please agree"));
     await browser.pause(3000);
   });
 });
